44694796e6b4bd9f9f67d2cb1e84663c26ec974a
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-01-04 16:34:42 +0900

    types

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log('Hello world');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'GI', age = 28, gender = 'male';
+const sayHello = (name, age, gender) => console.log(`${name}, Welcomes , You are ${age}  ${gender ? 'and ' + gender : ''}`);
+sayHello(name, age, gender);
 //# sourceMappingURL=index.js.map
